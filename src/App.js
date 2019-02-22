057762fdb7f2bb609e014200802e5805e11fb5f4
Ihor Dubii <ihor.dubii@fireblink.com>
2019-02-22 17:09:34 +0200

    fix problems with data flow

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CityList from './CityList'
-import Content from './Content'
 import axios from 'axios';
 import config from './config';
+import FilteredContent from "./FilteredContent";
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
 			cities: [
 				{
@@ -26,11 +26,11 @@ class App extends Component {
 				}]
 		}
 	}
-	
+
 	componentDidMount() {
 		this.state.cities.forEach(city => this.getInfo(city));
 	}
-	
+
 	addCity = (name) => {
 		const city = {
 			name
@@ -41,23 +41,26 @@ class App extends Component {
 			this.getInfo(city);
 		});
 	};
-	
+
 	render() {
 		return (
 			<div className="App">
 				<CityList cities={this.state.cities} onSubmit={this.addCity}/>
-				<Content cities={this.state.cities}/>
+                <FilteredContent cities={this.state.cities}/>
+
+
+				{/*<Content cities={this.state.cities}/>*/}
 			</div>
 		);
 	}
-	
+
 	async getInfo(city) {
 		const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city.name}&appid=${config.weatherAppId}&units=metric`);
-		
-		
+
+
 		city.temp = data.main.temp;
 		city.cloudIndex = data.clouds.all;
-		
+
 		this.setState({
 			cities: [...this.state.cities]
 		});
--- src/CityList.js
+++ src/CityList.js
@@ -3,28 +3,14 @@ import City from './City'
 import AddCity from './AddCity'
 
 class  CityList extends Component {
-	constructor(props) {
-		super(props);
-		
-		
-		
-		
-	}
-	
-	
-	
-	
 	render() {
-		
-		
 		return (
 			<div className="CityList">
 				<ul>
 					{ this.props.cities.map((city, key) => <City city={city} key={key}/>) }
 				</ul>
-				
+
 				<AddCity onSubmit={this.props.onSubmit} />
-				
 			</div>
 		);
 	}
