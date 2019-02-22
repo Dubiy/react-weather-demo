08d6ec103d4012a828a26ae036302569afe193a6
Ihor Dubii <ihor.dubii@fireblink.com>
2019-02-22 16:46:29 +0200

    fix problems with data flows

--- src/Filter.js
+++ src/Filter.js
@@ -3,35 +3,28 @@ import React, { Component } from 'react';
 class Filter extends Component {
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
 			filtered: this.props.cities
 		}
 	}
-	
-	static getDerivedStateFromProps(nextProps, prevState) {
-		console.log('helloo', nextProps, prevState);
-		// if (
-		// 	!prevState ||
-		// 	prevState.current !== nextProps.amount
-		// ) {
-		// 	return {
-		// 		previous: prevState.current,
-		// 		current: nextProps.amount
-		// 	};
-		// }
-	}
-	
+
+	componentWillReceiveProps(nextProps) {
+        if (nextProps.cities != this.props.cities) {
+            this.props.onFilter(nextProps.cities);
+        }
+    }
+
 	componentDidMount() {
 		this.props.onFilter(this.state.filtered);
 	}
-	
+
 	render() {
 		return (
 			<div>filter here</div>
 		)
 	}
-	
+
 }
 
 export default Filter;
